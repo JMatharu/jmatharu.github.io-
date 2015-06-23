4c7eaf5c4cf192170c7547f15e75617aa1ef40ff
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-06-23 12:54:19 -0400

    Adding Comments to Main PAge

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+var app = angular.module('chatApp', ['firebase']);
+
+app.controller('chatController', ['$scope', 'Message', function ($scope, Message) {
+
+    var defaultForm = {
+        name: "",
+        email: "",
+        comment: ""
+    };
+
+    $scope.user = "Guest";
+
+    $scope.messages = Message.all;
+
+    $scope.inserisci = function (message) {
+        Message.create(message);
+        $scope.newmessage = defaultForm;
+    };
+    //   $scope.getDatetime = new Date();
+ }]);
+
+app.factory('Message', ['$firebase',
+ function ($firebase) {
+        var ref = new Firebase('https://mainportfolio.firebaseio.com/');
+        var messages = $firebase(ref.child('messages')).$asArray();
+
+        var Message = {
+            all: messages,
+            create: function (message) {
+                return messages.$add(message);
+            },
+            get: function (messageId) {
+                return $firebase(ref.child('messages').child(messageId)).$asObject();
+            },
+            delete: function (message) {
+                return messages.$remove(message);
+            }
+        };
+
+        return Message;
+
+ }
+ ]);
